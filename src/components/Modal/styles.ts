109b745b846fbe2d7d8aec7a015ce529d567057e
Arthurk12 <arthur_bockmann@hotmail.com>
2026-06-03 22:43:50 -0300

    fix(BBBModal): make stickyFooter: false position footer below content
    When `stickyFooter` is set to false, the footer does not correctly
    position itself below the full modal content and is affected by
    additional padding.
    
    Position the footer below the entire modal content when
    `stickyFooter` is false, ignoring any additional padding.

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -53,13 +53,17 @@ export const CloseButton = styled.button`
   line-height: 1;
 `;
 
-export const ModalBody = styled.div<StyledModalBodyProps>`
+export const ModalScrollArea = styled.div<StyledModalBodyProps>`
   flex-grow: 1;
   overflow-y: ${({ $allowScroll }) => $allowScroll ? 'auto' : 'hidden'};
   overflow-x: hidden;
   display: flex;
   flex-direction: column;
-  padding: 0 ${spacingLarge} 0;
+`;
+
+export const ModalBodyContent = styled.div`
+  flex-grow: 1;
+  padding: 0 ${spacingLarge};
   margin: ${spacingSmallMedium} 0 ${spacingSmallMedium};
 `;
 
